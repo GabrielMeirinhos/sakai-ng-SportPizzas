1be1c86066633e654b7296099d286a729155c30f
Gabriel Meirinhos <gabriel.meirinhos3@gmail.com>
2024-11-10 16:58:47 -0300

    inicio projeto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
-export const environment = {
-  production: true
-};
+export const firebaseConfig = {
+    apiKey: "AIzaSyD9F2YA4aHh_7ld76K7Hw2E5B9axEl4JOk",
+    authDomain: "crud-sportpizzas.firebaseapp.com",
+    databaseURL: "https://crud-sportpizzas-default-rtdb.firebaseio.com",
+    projectId: "crud-sportpizzas",
+    storageBucket: "crud-sportpizzas.firebasestorage.app",
+    messagingSenderId: "325929883080",
+    appId: "1:325929883080:web:8e31bba370f64e7536dcfd",
+    measurementId: "G-NNSRYRR8HC"
+  };
