f8a0e9ebdf8aaf519174ff5f52de867c92c72680
Gabriel Meirinhos <gabriel.meirinhos3@gmail.com>
2024-11-27 17:53:14 -0300

    injetando environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig :{
-      apiKey: "AIzaSyD9F2YA4aHh_7ld76K7Hw2E5B9axEl4JOk",
-      authDomain: "crud-sportpizzas.firebaseapp.com",
-      databaseURL: "https://crud-sportpizzas-default-rtdb.firebaseio.com",
-      projectId: "crud-sportpizzas",
-      storageBucket: "crud-sportpizzas.firebasestorage.app",
-      messagingSenderId: "325929883080",
-      appId: "1:325929883080:web:8e31bba370f64e7536dcfd",
-      measurementId: "G-NNSRYRR8HC"
-    }
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  }
 }
